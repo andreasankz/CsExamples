5fea927c33a406a3f59348ad36080f4af1093f5b
Andreas Saarenpää <andreas.saarenpaa2@gmail.com>
2020-09-01 14:16:32 +0200

    C7

---
+++ C7_GetterAndSettersV2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C7_GetterAndSettersV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.AskForDateOfBirth();
+        }
+    }
+}
