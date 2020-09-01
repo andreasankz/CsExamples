63a69fc22a63ee641ee1a3a0aaba04ca632a260c
Andreas Saarenpää <andreas.saarenpaa2@gmail.com>
2020-09-01 15:02:12 +0200

    C8

---
+++ C8_Arrays/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace C8_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many siblings do you have? ");
+
+            var _numberOfSiblings = Convert.ToInt32(Console.ReadLine());
+
+            if (_numberOfSiblings < 1)
+                Console.WriteLine("Okey. That's cool man.");
+
+            else
+            {
+                string[] siblings = new string[_numberOfSiblings];
+
+                if(_numberOfSiblings == 1)
+                {
+                    Console.Write("What's your siblings name? ");
+                    siblings[0] = Console.ReadLine();
+                }
+                else
+                {
+                    for (var i = 0; i < _numberOfSiblings; i++)
+                    {
+                        if (i == 0)
+                            Console.Write("What's your first siblings name? ");
+                        else
+                            Console.Write("And your next siblings name? ");
+
+                        siblings[i] = Console.ReadLine();
+
+                    }
+                }
+
+                if (_numberOfSiblings == 1)
+                    Console.Write("So correct me if i am wrong. Your sibling is ");
+                else
+                    Console.Write("So correct me if i am wrong. Your siblings are  ");
+
+                foreach (var sibling in siblings)
+                {
+                    Console.Write($"{sibling} ");
+                }
+
+            }
+        }
+    }
+}
