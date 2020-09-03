5b69dd4b893f3f794e3a18ebd8a008c08c578180
Andreas Saarenpää <andreas.saarenpaa2@gmail.com>
2020-09-03 14:29:35 +0200

    C14

---
+++ C14_Abstract_1/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C14_Abstract_1.Models
+{
+    abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+
+    }
+}
---
+++ C14_Abstract_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using C14_Abstract_1.Models;
+using System;
+using System.Collections.Generic;
+
+namespace C14_Abstract_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classlist = new List<Person>()
+            {
+                
+
+                new Teacher { FirstName = "Andreas", LastName = "Kallesson", Role = "Teacher"},
+                new Student { FirstName = "Anna", LastName = "Andersson", },
+                new Student { FirstName = "Kalle", LastName = "Bondesson", }
+            };
+
+
+            foreach (var person in classlist)
+            {
+                if(person is Teacher teacher)
+                Console.WriteLine($"{teacher.FirstName} {teacher.LastName} - {teacher.Role}");
+
+                else
+                    Console.WriteLine($"{person.FirstName} {person.LastName}");
+            }
+
+
+
+            
+            
+            
+           
+            
+        }
+    }
+}
