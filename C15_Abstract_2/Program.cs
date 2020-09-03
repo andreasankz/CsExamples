00060149b658f2ce8b3ccdc7f01d73cefdc4ed84
Andreas Saarenpää <andreas.saarenpaa2@gmail.com>
2020-09-03 14:56:22 +0200

    C15

---
+++ C15_Abstract_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using C15_Abstract_2.Models;
+using System;
+
+namespace C15_Abstract_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            var tempsensor = new TempSensor();
+            Console.WriteLine(tempsensor.CreateFromConnectionString("HostName=iot.azure.com;DeviceId=dev1"));
+
+            Console.WriteLine();
+
+            var command = tempsensor.ReceiveMessage();
+            Console.WriteLine($"Received Command: {command}");
+
+            switch (command)
+            {
+                case "get":
+                    tempsensor.SendMessage("temperatur = 12");
+                    break;
+
+                case "reset":
+                    tempsensor.SendMessage("device has been reset.");
+                    break;
+
+                default:
+                    tempsensor.SendMessage("command not implemented");
+                    break;
+            }
+        }
+    }
+}
